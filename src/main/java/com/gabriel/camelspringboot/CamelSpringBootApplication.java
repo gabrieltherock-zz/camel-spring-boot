810cd3e745bc9deddf897d3d9a3aa894608c7419
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-03-27 13:05:35 -0300

    Initial commit

---
+++ src/main/java/com/gabriel/camelspringboot/CamelSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.camelspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelSpringBootApplication.class, args);
+    }
+
+}
