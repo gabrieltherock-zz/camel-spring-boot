ccc2ac7663d1f1def362249f1ebf33dc20209b51
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-03-29 17:17:12 -0300

    exemplo que integra um documento a uma fila do ActiveMQ

--- src/main/java/com/gabriel/camelspringboot/CamelSpringBootApplication.java
+++ src/main/java/com/gabriel/camelspringboot/CamelSpringBootApplication.java
@@ -1,13 +1,25 @@
 package com.gabriel.camelspringboot;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.component.activemq.ActiveMQComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CamelSpringBootApplication {
 
+    @Autowired
+    private CamelContext context;
+
+    @PostConstruct
+    public void init() throws Exception {
+        context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CamelSpringBootApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/gabriel/camelspringboot/OrderRoute.java
@@ -0,0 +1,14 @@
+package com.gabriel.camelspringboot;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("file:orders?noop=true")
+                .to("activemq:queue:orders");
+    }
+}
